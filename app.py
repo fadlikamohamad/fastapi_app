0ebd3c69675b0c0293602b8ab43246316b4ed567
fadlikamohamad <56854569+fadlikamohamad@users.noreply.github.com>
2021-09-30 20:29:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import re
 
-# def preProcess_data(text): #cleaning the data
+def preProcess_data(text): #cleaning the data
     
-#     text = text.lower()
-#     new_text = re.sub('[^a-zA-z0-9\s]','',text)
-#     new_text = re.sub('rt', '', new_text)
-#     return new_text
+    text = text.lower()
+    new_text = re.sub('[^a-zA-z0-9\s]','',text)
+    new_text = re.sub('rt', '', new_text)
+    return new_text
 
 app = FastAPI()
 
-# data = pd.read_csv('Sentiment.csv')
-# tokenizer = Tokenizer(num_words=2000, split=' ')
-# tokenizer.fit_on_texts(data['text'].values)
+data = pd.read_csv('Sentiment.csv')
+tokenizer = Tokenizer(num_words=2000, split=' ')
+tokenizer.fit_on_texts(data['text'].values)
 
 
 
-# def my_pipeline(text): #pipeline
-#   text_new = preProcess_data(text)
-#   X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
-#   X = pad_sequences(X, maxlen=28)
-#   return X
+def my_pipeline(text): #pipeline
+  text_new = preProcess_data(text)
+  X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
+  X = pad_sequences(X, maxlen=28)
+  return X
 
 
 @app.get('/') #basic get view
